5dd0dedc2d41bf4c3927e79face4a262553d6325
Mehakb77 <b7mehak@gmail.com>
2023-09-24 22:25:25 +0530

    initial setup connect to the connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,41 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+require("dotenv").config();
+
 const app = express();
+
+const connection = mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((m) => m.connection.getClient());
+
+const sessionStore = new MongoStore({
+  clientPromise: connection,
+  collection: "sessions",
+});
+
+app.use(
+  session({
+    secret: "Hheheh",
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello there nice to meet you");
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(4000, () => {
   console.log("server start listening to port");
