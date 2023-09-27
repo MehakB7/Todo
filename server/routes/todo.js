61b8d8e2e165d984731d4ea3dffd0d8ca6190722
Mehakb77 <b7mehak@gmail.com>
2023-09-27 21:02:02 +0530

    updated the routes

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -4,6 +4,7 @@ const {
   getTodos,
   getTodoById,
   deleteTodo,
+  updateTodo,
 } = require("../controller/todo");
 const { auth } = require("../middleware/auth");
 const router = express.Router();
@@ -12,6 +13,7 @@ router.post("/", auth, createTodo);
 router.get("/", auth, getTodos);
 router.get("/:id", auth, getTodoById);
 router.delete("/:id", auth, deleteTodo);
+router.put("/:id", auth, updateTodo);
 
 module.exports = {
   todoRouter: router,
